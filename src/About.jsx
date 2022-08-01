e6d0e615a28fae0e31ca791bd0c4bbbf3bf55380
Aakash Dixit <aakash26dixit@gmail.com>
2022-08-01 15:48:19 +0530

    FC

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <input type="text" placeholder="Text here"/>
+        </div>
+    )
+}
+
+export default About
+
